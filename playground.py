f51bdce93cdfa6e2d22847cbc01876bcbeb1fd4b
Naifen Mizuha <naphthol@foxmail.com>
2025-05-13 13:45:58 +0800

    delete useless files

--- playground.py
+++
@@ -1,9 +0,0 @@
-from utils.param import quantize, unquantize
-import torch
-
-tensor = torch.randn(1, 3, 224, 224, dtype=torch.float32)
-dic = {'a': tensor}
-print(dic)
-tq = quantize(dic)
-print(tq)
-print(unquantize(tq))
--- test/b.py
+++
@@ -1,6 +0,0 @@
-import galois
-
-gf = galois.GF(2**8)
-
-a = gf.Random(100000000).reshape([1000, -1])
-print(a.size)
